097774dfc01c308f39a04efaf340ec91f7ebc654
DominicWithK <aickdr1@student.op.ac.nz>
2026-09-22 11:05:53 +1200

    Added multiplication table

--- Program.cs
+++ Program.cs
@@ -8,16 +8,16 @@
             Console.ReadLine();
 
             //Dominik
-
-
-
-
-
-
-
-
-
-
+            int mult;
+            Console.Write("Multiplication tables: Insert a number to see multiples: ");
+            mult = Convert.ToInt32(Console.ReadLine());
+            int[] num = new int[20];
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = (i + 1) * mult;
+                Console.WriteLine($"{i + 1} x {mult} = {num[i]}");
+            }
+            Console.ReadLine();
 
 
 
